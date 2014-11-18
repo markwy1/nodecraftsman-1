77af1363272e2a5883deec1830e1229009d06509
Bob Gilmore <dev@bobgilmore.name>
2014-11-18 00:31:18 -0500

    Get basic test passing

--- src/greet.js
+++ src/greet.js
@@ -1,5 +1,10 @@
 'use strict';
 
-var greet = function() {};
+var greet = function(name) {
+  if (name == undefined) {
+    name = 'world';
+  }
+  return "Hello " + name + "!";
+};
 
 module.exports = greet;
